46638d7bd1dfc847fc513dfa5b5df3a4b0aa3868
Wang Zuo <wzuoadjusted@gmail.com>
2017-06-11 17:46:44 +0800

    update <Head />

--- packages/hello-world-example/components/Layout.js
+++ packages/hello-world-example/components/Layout.js
@@ -3,9 +3,20 @@ import React from 'react';
 import Head from 'cxx/head';
 import Link from 'cxx/link';
 
+const metas = [
+  { charset: 'utf-8' },
+  { httpEquiv: 'X-UA-Compatible', content: 'IE=edge' },
+  { name: 'renderer', content: 'webkit' },
+  {
+    name: 'viewport',
+    content:
+      'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no'
+  }
+];
+
 const Layout = ({ title, titleTemplate, children }) =>
   <div className={styles.layout}>
-    <Head titleTemplate={titleTemplate} title={title} />
+    <Head metas={metas} titleTemplate={titleTemplate} title={title} />
     <h1>Hello World Example</h1>
     <div className={styles.nav}>
       <Link href="/">Home</Link>
