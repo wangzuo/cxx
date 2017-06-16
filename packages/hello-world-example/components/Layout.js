d41230ad3f726ec905504b9f50b3a5475d9e08c1
Wang Zuo <wzuoadjusted@gmail.com>
2017-06-17 00:55:35 +0800

    meta fix

--- packages/hello-world-example/components/Layout.js
+++ packages/hello-world-example/components/Layout.js
@@ -3,20 +3,18 @@ import React from 'react';
 import Head from 'cxx/head';
 import Link from 'cxx/link';
 
-const metas = [
-  { charset: 'utf-8' },
-  { httpEquiv: 'X-UA-Compatible', content: 'IE=edge' },
-  { name: 'renderer', content: 'webkit' },
-  {
-    name: 'viewport',
-    content:
-      'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no'
-  }
-];
-
 const Layout = ({ title, titleTemplate, children }) =>
   <div className={styles.layout}>
-    <Head metas={metas} titleTemplate={titleTemplate} title={title} />
+    <Head titleTemplate={titleTemplate}>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta renderer="webkit" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+      />
+      <title>{title}</title>
+    </Head>
     <h1>Hello World Example</h1>
     <div className={styles.nav}>
       <Link href="/">Home</Link>
